4113ef980983f6b6d0a9a3b3c923a6a4e5ff3ecd
Erwan <erwanletutour.elt@gmail.com>
2019-11-03 16:16:33 +0100

    [V2] return of owl-carousel

--- src/main/resources/static/js/weatherV2.js
+++ src/main/resources/static/js/weatherV2.js
@@ -1,18 +1,19 @@
-$('.owl-carousel').owlCarousel({
-    items:1,
-    loop:true,
-    margin:10,
-    center:true,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
         }
-    }
-})
+    })
+});
